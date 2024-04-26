7956bbf9d700fa65343ffadac32f185e22480105
chisty sohel <chistysohail@gmail.com>
2024-04-26 15:33:32 -0400

    Add project files.

---
+++ DockerConsoleAppPoc/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DockerConsoleAppPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArgumentApp is running...");
+            while (true)
+            {
+                Console.Write("Enter arguments (type 'exit' to quit): ");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                var arguments = input.Split(' ');
+                Console.WriteLine("Arguments received:");
+                foreach (var arg in arguments)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
